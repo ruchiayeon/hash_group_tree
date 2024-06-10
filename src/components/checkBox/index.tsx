7d903a0dd406ef43a353ab3b3af351f21eff468b
ruchiayeon <ruchia10326@gmail.com>
2024-06-11 03:16:47 +0900

    [BUILD] 폴더 구조 형성 및 절대경로 설정

---
+++ src/components/checkBox/index.tsx
@@ -0,0 +1,3 @@
+export {default as appendCheckBox} from "@components/checkBox/append"
+export {default as basicheckBox} from "@components/checkBox/basic"
+export {default as exceptCheckBox} from "@components/checkBox/except"
